f11a2c42f4d5f9ae6d8e90f4a306b6b9d60161ab
Chunxuan Shao <chunxuan@outlook.com>
2022-11-25 17:55:43 +0100

    feat: scatter plot across plates with data.table

--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -5,41 +5,49 @@
 #'
 #' @import magrittr
 #' @import ggplot2
-#' @param dat synthetic lethal RNAi screen data
+#' @param dta synthetic lethal RNAi screen data
+#' @param scatter_colour colour for different signals
 #' @param controlOnly whether or not to plot control wells only
-#' @param colour colour for different signals
-#' @param ... positive/negative signals, must be specified
+#' @param control_name names of control siRNAs.
 #' @return a ggplot object
 #' @examples
 #' scatterPlot(exampleDat, controlOnly = FALSE, colour = rainbow(10),
 #'   "PLK1 si1", "scrambled control si1", "lipid only")
 #' @export
-scatterPlot <- function(dat, controlOnly = FALSE, colour, ...) {
-  if(class(dat) != "data.frame") stop("incorrect input!")
-  xx.1 <- as.character(dat[, "WELL_CONTENT_NAME"])
-  ## specific signals
-  if (controlOnly == FALSE) {
-    xx.1[grep(paste(c(...), collapse="|"), xx.1, invert=TRUE)] <- "knock-down signals"
-    ## location of controll wells;
-    data.f2 <- cbind(dat, condition = xx.1)
-    } else {
-    xx.control <- grep(paste(c(...), collapse="|"), xx.1)
-    xx.2 <- xx.1[xx.control]
-    data.f2 <- cbind(dat[xx.control, ], condition = xx.2)
-}
+# scatterPlot <- function(dta, colour = rainbow(10), controlOnly = FALSE, ...) {
+scatterPlot <- function(dta, scatter_colour = rainbow(10), controlOnly = FALSE, control_name = NULL) {
+  dta_2 <- copy(dta)
 
-  data.f2 <- data.f2[order(data.f2$EXPERIMENT_MODIFICATION, decreasing = TRUE),]
-  data.f2$PLATE <- factor(data.f2$PLATE)
-  xx.1 <- paste(data.f2$PLATE, data.f2$EXPERIMENT_MODIFICATION, sep = " ") %>%
-    factor
-  data.f2 <- cbind(data.f2, names = xx.1)
-  ggplot(data.f2, aes(names, READOUT, colour = condition)) +
+  if (controlOnly == FALSE) {
+    if (is.null(control_name)) {
+      message("(II) Better to provide names for positive/negative controls.")
+      dta_2[, new_cond := "knock-down signals"]
+    } else {
+      dta_2[, new_cond := WELL_CONTENT_NAME] %>%
+        .[!(WELL_CONTENT_NAME %in% control_name), new_cond := "knock-down signals"]
+    }
+  } else {
+    if (is.null(control_name)) {
+      stop("(EE) Please provide names for positive/negative controls if controlOnly is TRUE.")
+    } else {
+      dta_2 <- dta_2[WELL_CONTENT_NAME %in% control_name][, new_cond := WELL_CONTENT_NAME]
+    }
+  }
+
+  dta_2 <- dta_2[order(EXPERIMENT_MODIFICATION)] %>%
+    .[, new_name := paste(PLATE, EXPERIMENT_MODIFICATION, sep = " ") %>% factor]
+
+  ggplot(as.data.frame(dta_2), aes(new_name, READOUT, colour = new_cond)) +
     geom_point() +
-    theme_bw() +
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-    theme(axis.title = element_text(face = "bold")) +
-    theme(axis.text.x = element_text(angle = -90, hjust = 0)) +
-    labs(y = "READOUTS\n") +
-    theme(legend.position = c(1,1), legend.justification = c(1,1)) +
-    scale_colour_manual(values = colour)
+    theme_bw(14) +
+    theme(panel.grid.major     = element_blank(),
+          panel.grid.minor     = element_blank(),
+          panel.background     = element_rect(colour = "black", size = 1),
+          axis.title           = element_text(face = "bold"),
+          axis.text.x          = element_text(angle = -90, hjust = 0),
+          legend.position      = c(1, 1),
+          legend.title         = element_blank(),
+          legend.justification = c(1, 1)) +
+    labs(x = "", y = "READOUTS\n") +
+    scale_colour_manual(values = scatter_colour)
 }
