9b8877d0c78aeac19a86389545ecfc46d2fe0e8d
Chunxuan Shao <chunxuan@outlook.com>
2022-12-03 20:38:18 +0100

    fix: dependency

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,11 @@
 #' @import data.table
 #' @import ggplot2
 #' @import patchwork
-#' @importFrom magrittr %>% set_rownames set_colnames set_names set_rownames extract
+#' @importFrom magrittr %>% set_rownames set_colnames set_names set_rownames extract inset
+#' @importFrom grDevices colorRampPalette rainbow
+#' @importFrom methods is
+#' @importFrom utils write.table
+#' @importFrom stats mad median medpolish na.omit p.adjust phyper sd t.test
 NULL
 
 #- deal with . in magrittr
