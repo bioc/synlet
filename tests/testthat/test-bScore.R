6bb92cd10f93a11b42d5c0a27bcb84157245aaea
Chunxuan Shao <chunxuan@outlook.com>
2022-12-06 16:43:52 +0100

    fix: bscore example Test the new and prev codes on example_dt, identical results.

--- tests/testthat/test-bScore.R
+++ tests/testthat/test-bScore.R
@@ -1,18 +1,11 @@
 context("Test output of bscore and t-test")
 
 test_that("b-score is correctly calculated", {
-  bscore.res <- sapply("P001", bScore, exampleDat, 
-    control = "control", treatment = "treatment", simplify = FALSE)
-  bscore.res.1 <- bscore.res$P001
-  expect_equal(as.numeric(bscore.res.1[1, ]), c(-0.3888287, -0.6491217, 
-    0.1497299, 1.5912198, 1.0046037, -1.5310097), tolerance = 1e-3)
-  bscore.ttest  <- sapply(names(bscore.res), tTest, bscore.res, 
-      numTreat = 3, numCont = 3, simplify = FALSE, USE.NAMES = TRUE)
-  expect_equal(as.numeric(bscore.ttest$P001[1, ]),
-      c(0.5706962, -0.6510114, 0.8836586), tolerance = 1e-3)
-
+  res <- sapply("P001", bScore, example_dt, control = "control",
+                       treatment = "treatment", simplify = FALSE)
+  res_1 <- res$P001
+  expect_equal(as.numeric(res_1[1, ]), c(-0.379059117,  1.4344181, -0.8484694,
+                                         -0.1088711, -1.14224946, -0.0854374,), tolerance = 1e-3)
+  ttest_res  <- tTest(res_1, 3, 3)
+  expect_equal(as.numeric(ttest_res[1, ]), c(0.55670002,  0.51448255, 0.8761181), tolerance = 1e-3)
 })
-
-
-
-
