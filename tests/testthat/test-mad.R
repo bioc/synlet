928afa5d5c59b4ce88c7265f2b9e16a52379dce8
Chunxuan Shao <chunxuan@outlook.com>
2022-12-07 10:03:14 +0100

    fix: mad passed

--- tests/testthat/test-mad.R
+++ tests/testthat/test-mad.R
@@ -1,16 +1,7 @@
 context("Test median +- k*MAD")
 
 test_that("Correct calculation of median +- k*MAD", {
-  madSelection <- sapply("P001", madSelect, exampleDat,
-    control = "control", treatment = "treatment", simplify = FALSE)
-  expect_equal(as.numeric(madSelection$P001[1, 2:4]),
-      c(0.8966, 0.8981399, 1.001757), tolerance = 1e-3)
-  expect_equal(as.character(madSelection$P001[1, 5]), "No")
-  expect_equal(rownames(madSelection$P001)[1], "AAK1 si3")
+  res <- madSelect("P001", example_dt, control = "control", treatment = "treatment")
+  expect_equal(as.numeric(res[1, 2:4]), c(0.9427558, 1.0201578, 1.0821018), tolerance = 1e-3)
+  expect_equal(rownames(res)[1], "A2M si1")
 })
-
-
-
-
-
-
