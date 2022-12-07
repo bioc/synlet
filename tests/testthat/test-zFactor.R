b1170006d1aff22d7c588713864b5b0722c4e7f5
Chunxuan Shao <chunxuan@outlook.com>
2022-12-07 10:07:58 +0100

    fix: zFactor test passed

--- tests/testthat/test-zFactor.R
+++ tests/testthat/test-zFactor.R
@@ -1,15 +1,6 @@
 context("Test calculation of z factor")
 
 test_that("Correct calculation of z factor", {
-  res <- zFactor(exampleDat, negativeCon = "scrambled control si1",
-    positiveCon = "PLK1 si1")
-  expect_equal(as.numeric(res[1, ]), c(-10.63422, -41.36317), tolerance = 1e-3)
+  res <- zFactor(example_dt, negativeCon = "scrambled control si1", positiveCon = "PLK1 si1")
+  expect_equal(as.numeric(res[1, ]), c(-10.634224, -41.363250), tolerance = 1e-3)
 })
-
-
-
-
-
-
-
-
